3c74ca883c4670596221fd4cba43517845c4a431
Blaise <ifollowjre@gmail.com>
2020-02-25 18:39:20 -0700

    added ormSelect ormInsert ormUpdate

--- config/orm.js
+++ config/orm.js
@@ -1,39 +1,45 @@
 var connection = require("./connection.js");
-
+const cTable = require('console.table');
 
 var orm = {
-    selectAll: function(whatToSelect, tableInput) {
-      var queryString = "SELECT * FROM ??";
-      connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+  selectAll: function (whatToSelect, tableInput) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [whatToSelect, tableInput], function (err, result) {
+      if (err) throw err;
+      console.table(result);
+    });
+  },
+  // insertOne: function (tableInput, colToSearch) {
+  //   var queryString = "INSERT INTO ?? (burgers, burger_name) VALUES ('??', false)";
+
+  //   // console.log(queryString);
+
+  //   connection.query(queryString, [tableInput, colToSearch], function (err, result) {
+  //     if (err) throw err;
+  //     // console.log(result);
+  //     console.table(result);
+  //   });
+  // },
+
+  insertOne: function (burgerName) {
+
+    var query = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)";
+    //INSERT INTO burgers (id, burger_name, devoured) VALUES ('Hot-Mama', true);
+    connection.query(query, [burgerName, 0], function (err, result) {
+      if (err) throw err;
+      console.log("Add complete");
+      console.log(result);
+    })
+    
+  },
+  updateOne: function (burgerName, id) {
+    var query = "UPDATE burgers SET burger_name = ?, devoured = false WHERE burger_id = ?;"
+    connection.query(query, [burgerName, id], function (err, result) {
         if (err) throw err;
+        console.log("Update complete");
         console.log(result);
-      });
-    },
-    selectWhere: function(tableInput, colToSearch, valOfCol) {
-      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    leftJoin: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
-      var queryString = "SELECT ?? FROM ?? AS tOne";
-      queryString += " LEFT JOIN ?? AS tTwo";
-      queryString += " ON tOne.?? = tTwo.??";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function(
-        err,
-        result
-      ) {
-        if (err) throw err;
-        console.log(result);
-      });
-    }
-  };
-  
-  module.exports = orm;
+    })
+}
+};
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
-
+var orm = require("./config/orm.js");
 var PORT = process.env.PORT || 8080;
-
+var exphbs = require("express-handlebars")
 var app = express();
-
+const cTable = require('console.table');
 
 app.use(express.static("public"));
 
@@ -15,7 +15,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-
+// orm.insertOne("Fat");
+orm.selectAll("burgers");
+// orm.updateOne("BooBoo",15);
 app.listen(PORT, function () {
     console.log("App now listening at localhost:" + PORT);
 });
